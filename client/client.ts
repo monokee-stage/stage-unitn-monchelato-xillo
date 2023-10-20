532b7b94a2619c7306f6f54cc886c70aac5469ab
Ennio Monchelato <95639617+ilmonk@users.noreply.github.com>
2023-10-20 09:24:55 +0200

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,21 +1,47 @@
 import axios from "axios";
 import { json } from "body-parser";
 import { UUID } from "mongodb";
-import { Request } from "express";
+import { Request, Response } from 'express';import { publicDecrypt } from "crypto";
+;
 
-type Stream_config = {
-    delivery:{
-      method: string;
-      endpoint_url: string;
-    };
-    events_requested: string;
-  };
+type Stato = "enabled"|"disabled"|"paused";
 
-type List_of_endpoints = {
+interface Client_config {
+  client_id:string;
+  events_requested: string[];
+  events_delivered: string[]; 
+  endpoint_delivery_url: string;
+  delivery: string;
+  delivery_method: string;
+  aud:string[];
+  status:Stato;
+  events: RequestInit[];
+  index: number;
+}
 
-  configure_stream: string;
-  add_subject: string;
-  request_verification: string;
+interface List_of_endpoints {
+
+  configuration_endpoint:
+        string,
+  add_subject_endpoint: 
+      string,
+  remove_subject_endpoint: 
+      string,
+  verification_endpoint: 
+      string,
+  status_endpoint: 
+      string,
+  issuer: 
+      string,
+  jwks_uri: 
+      string,
+  delivery_methods_supported: [
+      string,          
+      string          
+  ],
+  critical_subject_members: [
+      string
+  ]
 }
 
 export class Client
@@ -24,14 +50,7 @@ export class Client
   public audience;
   public auth;
   public verify;
-  public endpoints: List_of_endpoints;
-  public message: Stream_config = {
-    delivery:{
-      method: "string",
-      endpoint_url: "string",
-    },
-    events_requested: "string",
-  }
+  public endpoints;
 
 
   public constructor(transmitter_hostname: string, audience: string, bearer: string, verify: boolean = true)
@@ -41,105 +60,194 @@ export class Client
     this.auth = bearer;
     this.verify = verify;
     this.endpoints = {
-      configure_stream: '/configure-stream',
-      add_subject: '/add-subject',
-      request_verification: '/request-verification',
+
+      configuration_endpoint:
+            "",
+      add_subject_endpoint: 
+          "",
+      remove_subject_endpoint: 
+          "",
+      verification_endpoint: 
+          "",
+      status_endpoint: 
+          "",
+      issuer: 
+          "",
+      jwks_uri: 
+          "",
+      delivery_methods_supported: [
+          "",          
+          ""          
+      ],
+      critical_subject_members: [
+          ""
+      ]
     };
     
   }
 
-  public async get_endpoints(endpoint: string) //preimposto degli endpoints 
+  /*public async get_endpoints(endpoint: string) //get
   {
     const { data, status } = await axios.get<List_of_endpoints>(
       endpoint,
       {
-        headers: {
-          Accept: 'application/json',
-        },
-      },
+        headers:
+        {
+          'Authorization': `Bearer ${this.auth}`
+        }
+      }
     );
-    
-    this.endpoints = data;  
-  }
 
-  public async configure_stream(endpoint:string) //post
+    this.endpoints = (data as List_of_endpoints); 
+
+    console.log(status);
+  }*/
+
+  public async configure_stream() //post
   {
-    const { data, status } = await axios.post<Stream_config>(
-      endpoint,
-      this.message,
+    const { data, status } = await axios.post<Client_config>(
+      this.endpoints.configuration_endpoint,      //controlla sia l'endpoint giusto
+      {
+        delivery:{
+          method: "https://schemas.openid.net/secevent/risc/delivery-method/push",
+          endpoint_url: "string",
+        },
+        events_requested: "https://schemas.openid.net/secevent/risc/event-type/credential-compromise",
+      },
       {
         headers: {
           'Content-Type': 'application/json',
           Accept: 'application/json',
+          'Authorization': `Bearer ${this.auth}`
         },
+        
       },
     );
     console.log(status);
   }
 
-  public async add_subject(endpoint:string,subject:JSON) //post
+  public async add_subject(subject:JSON) //post
   {
-    const { data, status } = await axios.post<JSON>(    //da cambiare in or dei vari subject
-        endpoint,
+    const { data, status } = await axios.post<JSON>(          
+        this.endpoints.add_subject_endpoint,
         subject,
         {
           headers: {
             'Content-Type': 'application/json',
             Accept: 'application/json',
+            'Authorization': `Bearer ${this.auth}`
+          },
+        }
+      );
+      console.log(status);
+
+  }
+
+  public async remove_subject(subject:JSON) //post
+  {
+    const { data, status } = await axios.post<JSON>(          
+        this.endpoints.remove_subject_endpoint,
+        subject,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            'Authorization': `Bearer ${this.auth}`
           },
         },
       );
       console.log(status);
   }
 
-  public async request_verification(endpoint:string) //post
+  public async request_verification() //post
   {
-    const { data, status } = await axios.post<number>(
-        endpoint,
-         15 ,
+    const { data, status } = await axios.post(
+        this.endpoints.verification_endpoint,
+           
         {
           headers: {
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json',//da mettere bearer token
             Accept: 'application/json',
+            'Authorization': `Bearer ${this.auth}`
           },
         },
       );
       console.log(status,data);
   }
+
+  public async get_status(endpoint: string) //get
+  {
+    const { data, status } = await axios.get(
+      this.endpoints.status_endpoint
+    );
+    
+    console.log(data);
+  }
+
+  public async poll(endpoint:string){
+    const { data, status } = await axios.post<string>(
+      endpoint,
+       {
+        'maxevents': 5,
+        'returnImmediately': true,
+       } , //cambia in json da inviare
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          'Authorization': ''
+        },
+      },
+    );
+    console.log(status,data);
+  }
+
+  public async inviaRichiestaAlServer(endpoint:string) {
+    let tentativi = 0;
+
+    while (true) {
+      try {
+        
+        const { data, status } = await axios.get<List_of_endpoints>(
+          endpoint,
+          {
+            headers:
+            {
+              'Authorization': `Bearer ${this.auth}`
+            }
+          }
+        );
+
+        // Se la risposta è positiva, interrompi il loop
+        if (status === 200) {
+          this.endpoints = (data as List_of_endpoints); 
+          console.log(this.endpoints.add_subject_endpoint);
+          console.log('Il server ha risposto correttamente:', status);
+          break;
+        }
+      } catch (error) {
+        if (error instanceof Error)
+        // Gestisci gli errori, ad esempio timeout della richiesta
+        console.error('Errore durante la richiesta:', error.message);
+      }
+
+      // Incrementa il numero di tentativi
+      tentativi++;
+
+      // Attendi un po' prima di fare il prossimo tentativo (ad esempio, 1 secondo)
+      await new Promise(resolve => setTimeout(resolve, 10000));
+
+      console.log(`Tentativo ${tentativi} di contattare il server...`);
+    }
+    
+    }
 }
 
 export function pushEvents(req:Request) {
-  
-  let i=req.body[0];
-  console.log(i);
+
+  req.body.forEach(function (value: any) {
+    console.log(value);
+  })
 
 }
 
-export async function inviaRichiestaAlServer() {
-  let tentativi = 0;
-
-  while (true) {
-    try {
-      
-      const response = await axios.post('http://localhost:3000/events/poll');
-
-      // Se la risposta è positiva, interrompi il loop
-      if (response.status === 200) {
-        console.log('Il server ha risposto correttamente:', response.data);
-        break;
-      }
-    } catch (error) {
-      if (error instanceof Error)
-      // Gestisci gli errori, ad esempio timeout della richiesta
-      console.error('Errore durante la richiesta:', error.message);
-    }
-
-    // Incrementa il numero di tentativi
-    tentativi++;
-
-    // Attendi un po' prima di fare il prossimo tentativo (ad esempio, 1 secondo)
-    await new Promise(resolve => setTimeout(resolve, 10000));
-
-    console.log(`Tentativo ${tentativi} di contattare il server...`);
-  }
-}
